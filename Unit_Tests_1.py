39876e6cd20e7f430ad5e77244bf3a719be1523e
Aleksander-Mass <103246329+Aleksander-Mass@users.noreply.github.com>
2024-11-05 13:24:56 +0300

    Homework on the topic "Simple Unit Tests"
    Домашнее задание по теме "Простые Юнит-Тесты"
    
    Если вы решали старую версию задачи, проверка будет производиться по ней.
    Ссылка на старую версию тут.
    Цель: приобрести навык создания простейших Юнит-тестов
    
    Задача "Проверка на выносливость":
    
    В первую очередь скачайте исходный код, который нужно обложить тестами с GitHub. (Можно скопировать)
    В этом коде сможете обнаружить класс Runner, объекты которого вам будет необходимо протестировать.
    
    Напишите класс RunnerTest, наследуемый от TestCase из модуля unittest. В классе пропишите следующие методы:
    
    test_walk - метод, в котором создаётся объект класса Runner с произвольным именем. Далее вызовите метод walk у этого объекта 10 раз. После чего методом assertEqual сравните distance этого объекта со значением 50.
    test_run - метод, в котором создаётся объект класса Runner с произвольным именем. Далее вызовите метод run у этого объекта 10 раз. После чего методом assertEqual сравните distance этого объекта со значением 100.
    test_challenge - метод в котором создаются 2 объекта класса Runner с произвольными именами. Далее 10 раз у объектов вызываются методы run и walk соответственно. Т.к. дистанции должны быть разными, используйте метод assertNotEqual, чтобы убедится в неравенстве результатов.
    Запустите кейс RunnerTest. В конечном итоге все 3 теста должны пройти проверку.
    
    Пункты задачи:
    
    Скачайте исходный код для тестов.
    Создайте класс RunnerTest и соответствующие описанию методы.
    Запустите RunnerTest и убедитесь в правильности результатов.
    Пример результата выполнения программы:
    Вывод на консоль:
    Ran 3 tests in 0.001s OK
    
    Примечания:
    Попробуйте поменять значения в одном из тестов, результаты
    Файл tests_12_1.py с классом тестов загрузите на ваш GitHub репозиторий. В решении пришлите ссылку на него.

---
+++ Unit_Tests_1.py
@@ -0,0 +1,74 @@
+# Пункты задачи:
+# 1. Скачайте исходный код для тестов.
+####       # изменение с 10 на 15
+
+import unittest
+
+
+# Исходный класс Runner
+class Runner:
+    def __init__(self, name):
+        self.name = name
+        self.distance = 0
+
+    def run(self):
+        self.distance += 15  ####     изменение с 10 на 15
+
+    def walk(self):
+        self.distance += 5
+
+    def __str__(self):
+        return self.name
+
+# 2. Создайте класс RunnerTest и соответствующие описанию методы.
+# Тестовый класс для класса Runner
+class RunnerTest(unittest.TestCase):
+
+    def test_walk(self):
+        runner = Runner("TestRunner")
+        for _ in range(10):
+            runner.walk()
+        # Проверяем, что после 10 шагов distance равен 50
+        self.assertEqual(runner.distance, 50)
+
+    def test_run(self):
+        runner = Runner("TestRunner")
+        for _ in range(10):
+            runner.run()
+        # Проверяем, что после 10 пробежек distance равен 100
+        self.assertEqual(runner.distance, 100)
+
+    def test_challenge(self):
+        runner1 = Runner("Runner1")
+        runner2 = Runner("Runner2")
+
+        # runner1 использует run, runner2 использует walk
+        for _ in range(10):
+            runner1.run()
+            runner2.walk()
+
+        # Проверяем, что дистанции разные
+        self.assertNotEqual(runner1.distance, runner2.distance)
+
+
+# 3. Запуск тестов
+if __name__ == "__main__":
+    unittest.main()
+
+#####   Вывод на консоль:
+
+# Ran 3 tests in 0.010s
+#
+# FAILED (failures=1)
+#
+#
+# 100 != 150
+#
+# Expected :150
+# Actual   :100
+# <Click to see difference>
+#
+# Traceback (most recent call last):
+#   File "C:\Users\alexa\PycharmProjects\Module_12_Cycle_Support\Unit_Tests_1.py", line 38, in test_run
+#     self.assertEqual(runner.distance, 100)
+# AssertionError: 150 != 100
